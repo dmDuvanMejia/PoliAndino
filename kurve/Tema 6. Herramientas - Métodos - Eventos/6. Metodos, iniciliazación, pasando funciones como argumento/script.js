6519f4570cfb081d2d1d6645687f49c5e798ae9d
dquiros <dquiros@ospinternational.com>
2022-01-10 15:46:09 -0500

    Callback

--- kurve/Tema 6. Herramientas - Métodos - Eventos/6. Metodos, iniciliazación, pasando funciones como argumento/script.js
+++ kurve/Tema 6. Herramientas - Métodos - Eventos/6. Metodos, iniciliazación, pasando funciones como argumento/script.js
@@ -8,3 +8,28 @@ function Persona(nombre) {
 
 var persona1 = new Persona("Duvan Mejia");
 console.log(persona1.nombre);
+
+function mostrarNombre( callback ) {
+    let nombre = "Duvan Mejia";
+    callback(nombre);
+}
+
+function saludar( nombre ) {
+    console.log("Hola " + nombre);
+}
+
+mostrarNombre( saludar );
+
+var seconds = 20;
+function startCount() {
+    setInterval(function() {
+        seconds--;
+        showSeconds();
+    }, 1000);
+}
+
+function showSeconds() {
+    console.log( seconds );
+}
+
+startCount();
